5db30cc91526c77bb3c21c6f678c477fb497092a
Joergenator <joergentro@hotmail.com>
2025-04-28 12:51:21 +0200

    Byttet versjon på build av windows forms.

--- FrontDesk/Form1.Designer.cs
+++ FrontDesk/Form1.Designer.cs
@@ -1,38 +1,28 @@
-﻿namespace FrontDesk
+﻿using System.Windows.Forms;
+namespace FrontDesk
 {
-    partial class Form1
+    public partial class Form1 : Form
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private readonly AppDbContext _dbContext;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        public Form1()
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+            InitializeComponent();
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
+        public Form1(AppDbContext dbContext)
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            InitializeComponent();
+            _dbContext = dbContext;
+            LoadMaintenanceTasks();
+        }
+
+        private void LoadMaintenanceTasks()
+        {
+            // ...
         }
     }
 }
+
+
 
