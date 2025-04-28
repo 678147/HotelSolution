8f77bef06b2c778d25116dbb1a887e8611b2b566
678117 <678117@stud.hvl.no>
2025-04-28 11:42:43 +0200

    login

---
+++ CustomerWebApp/Pages/registrer.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CustomerWebApp.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public string Email { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        [BindProperty]
+        public string ConfirmPassword { get; set; }
+
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            if (Password != ConfirmPassword)
+            {
+                Message = "Passordene stemmer ikke overens.";
+                return;
+            }
+
+            // TODO: Lagre brukeren i databasen
+
+            Message = "Registrering vellykket! Du kan nå logge inn.";
+        }
+    }
+}
